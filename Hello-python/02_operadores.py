cd9cc5de125c88dab2f7b0874230d37783e25c4c
Juanse <juansesosa45@gmail.com>
2024-09-03 13:49:06 -0300

    class 2

---
+++ Hello-python/02_operadores.py
@@ -0,0 +1,49 @@
+# Operadores Aritmeticos
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 % 2)
+print(10 % 3)
+
+print(10 // 3)
+print(2 ** 3)
+
+print("Hola " + "Mundo ")
+print("Hola " + str(5))
+print("Hola " * 2)
+
+# Operadores Comparativos
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+print(3 > 4 == 2)
+
+print("Hola" > "Mundo")
+print("Hola" < "Mundo")
+print("Hola" >= "Mundo")
+print("Hola" <= "Mundo")
+print("Hola" == "Mundo")
+print("Hola" != "Mundo")
+
+
+# Operadores Logicos
+
+print(3 > 4 and "Hola" > "Mundo")
+print(3 > 4 or "Hola" > "Mundo")
+
+print(3 < 4 and "Hola" < "Mundo")
+print(3 < 4 or "Hola" > "Mundo")
+
+print(not (3 > 4))
+
+
+
+
+
+
