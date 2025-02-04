76bc9374240fb9941cfdbc695213f5e5205be65e
juanse <juansesosa45@gmail.com>
2025-02-04 13:38:55 -0300

    catalogo peliculas

---
+++ catalogo_peliculas/test_catalogo_peliculas.py
@@ -0,0 +1,28 @@
+from dominio.pelicula import Pelicula
+from servicio.catalogo_peliculas import CatalogoPeliculas
+
+opcion = None
+
+while opcion != 4:
+  try:
+    print("Opciones: ")
+    print("1. Agregar pelicula ")
+    print("2. Listar peliculas ")
+    print("3. Eliminar catalogos peliculas ")
+    print("4. Salir ")
+    opcion = int(input("Escribe tu opcion (1-4): "))
+  
+    if opcion == 1:
+      nombre_pelicula = input("Proporciona el nombre de la pelicula: ")
+      pelicula = Pelicula(nombre_pelicula)
+      CatalogoPeliculas.agregar_pelicula(pelicula)
+    elif opcion == 2:
+      CatalogoPeliculas.listar_pelicular()
+    elif opcion == 3:
+      CatalogoPeliculas.eliminar_peliculas() 
+        
+  except Exception as e:
+    print(f"Ocurrio un error {e}")
+    opcion = None
+else: 
+  print("Saliendo del programa...")
